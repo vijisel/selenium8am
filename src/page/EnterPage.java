f9824cce0b225a732d3a705e31a5e2e7ce414369
VijiT1985 <t.vijji@gmail.com>
2021-09-27 08:15:12 +0530

    uploading framework

---
+++ src/page/EnterPage.java
@@ -0,0 +1,51 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+
+import generic.AutoUtils;
+import generic.IAutoConstants;
+
+public class EnterPage 
+{
+	@FindBy(xpath="//div[contains(text(),'Help')]")
+	private WebElement help;
+	@FindBy(linkText="About your actiTIME")
+	private WebElement aboutAT;
+	@FindBy(xpath="//span[@class='productVersion']")
+	private WebElement version;
+	
+	public EnterPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	//Methods
+	public void clickHelp()
+	{
+		help.click();
+	}
+	public void clickAboutActiTIME()
+	{
+		aboutAT.click();
+	}
+	public void verifyProductVersion(String ExpectedVersion)
+	{
+		String ActualVersion = version.getText();
+		Assert.assertEquals(ActualVersion, ExpectedVersion);
+		Reporter.log("Actual and Expected versions are same",true);
+	}
+	public void verifyHomePageDisplayed(WebDriver driver, String eTitle)
+	{
+		String aTitle = driver.getTitle();
+		Assert.assertEquals(aTitle, eTitle);
+		Reporter.log("Actual tiltle is matching with the Expected tiltle",true);
+		Reporter.log("Actual tiltle is matching with the Expected tiltle",true);
+		
+	}
+}
